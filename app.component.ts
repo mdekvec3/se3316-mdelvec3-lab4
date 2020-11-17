1fbac39a3cf8c4bdd394e32ed05b686df4b08772
mdekvec3 <71351525+mdekvec3@users.noreply.github.com>
2020-11-16 19:28:27 -0500

    Input improvements and other small changes
    - validating course id isn't longer than 5 characters
    - setting input text fields to empty after submits
    - UI cleanup and arrangement

--- app.component.ts
+++ app.component.ts
@@ -7,19 +7,19 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class ConfigService {
-  private url: string = "http://localhost:3000/api/courseData";
+  private queryString: string = "http://localhost:3000/api/courseData";
 
   constructor(private http: HttpClient) {}
-   getcourses(): Observable<courseObject[]> {
-     return this.http.get<courseObject[]>(this.url);
-   }
-  getData(){};
-  searchSubmitted(){};
-  renderTimeTable(){};
-  addCoursesToSchedule(){};
-  scheduleSelected(scheduleName: string){};
-  createSchedule(){};
-  scheduleNameInput;
+    getcourses(): Observable<courseObject[]> {
+      return this.http.get<courseObject[]>(this.queryString);
+    };
+    getData(){};
+    searchSubmitted(){};
+    renderTimeTable(){};
+    addCoursesToSchedule(){};
+    scheduleSelected(scheduleName: string){};
+    createSchedule(){};
+    scheduleNameInput;
 }
 
 @Component({
@@ -29,13 +29,15 @@ export class ConfigService {
 })
 export class AppComponent {
 
-  ngOnInit(){};
-  // selectValue;
+  ngOnInit(){
+    console.log("on init called");
+    this._configservice.getcourses().subscribe(data => this.dataArray = data);
+  };
+
   title = 'lab4-angular';
   show = true;
   checkboxValue: boolean = false;
   scheduleNameInput: string; // initalizing to "" will cause errors: schedules will never have names
-
   showInfoTable = false; // info table is rendered only when this is true
   showTimeTable = false; // time table is rendered only when this is true
   renderedSchedule;
@@ -43,205 +45,34 @@ export class AppComponent {
   data: { [key: string]: any } = {};
   checked: boolean[] = [];
   objectKeys = Object.keys;
-
   activeSchedule: object[] = []; // giving errors
   actvieScheduleName: string;
   selectedCourses: object[] = []; // array of SELECTED courses (full data)
   scheduleData: any = {};
   timeBasedSchedule;
-
-  // for searching data
   dataArray;
-  matchingCourses;
 
   constructor(private _configservice:ConfigService){
-  
     this.showInfoTable = false;
     this.showTimeTable = false;
     this.checked = [];
     this.scheduleNameInput = "";
     this.actvieScheduleName = "";
-    // this.selectValue = "";
     this.timeBasedSchedule = {"8:30 AM": {},"9:30 AM": {},"10:30 AM": {},"11:30 AM": {},"12:30 PM": {},"1:30 PM": {},"2:30 PM": {},"3:30 PM": {},"4:30 PM": {},"5:30 PM": {},"6:30 PM": {},"7:30 PM": {},"8:30 PM": {},"9:30 PM": {}};
     this.renderedSchedule = "";
-
     this.dataArray = [];
-    // dataArray = courses
-    this.matchingCourses = [];
-
   }
 
   getData(){
-    this.matchingCourses = [];
+    let matchingCourses = [];
+    let courseData = this.dataArray;
 
-    // make http request to get data object
-    this._configservice.getcourses().subscribe(timetableData => this.dataArray = timetableData);
+    // getting search parameters
+      let selectedComponent = (document.getElementById("componentDropdown") as HTMLInputElement).value;
+      let courseId = (document.getElementById("courseNumber") as HTMLInputElement).value;
+      let subjectInput = (document.getElementById("subjectDropDown") as HTMLInputElement).value;  
 
-    // ignore
-    let jsonData = [
-      {
-        "catalog_nbr": "1021B",
-        "subject": "ACTURSCI",
-        "className": "INTRO TO FINANCIAL SECURE SYS",
-        "course_info": [
-          {
-            "class_nbr": 5538,
-            "start_time": "8:30 AM",
-            "descrlong": "",
-            "end_time": "9:30 AM",
-            "campus": "Main",
-            "facility_ID": "PAB-106",
-            "days": [
-              "M",
-              "W",
-              "F"
-            ],
-            "instructors": [],
-            "class_section": "001",
-            "ssr_component": "LEC",
-            "enrl_stat": "Not full",
-            "descr": "RESTRICTED TO YR 1 STUDENTS."
-          }
-        ],
-        "catalog_description": "The nature and cause of financial security and insecurity; public, private and employer programs and products to reduce financial insecurity, including social security, individual insurance and annuities along with employee pensions and benefits.\n\nExtra Information: 3 lecture hours."
-      },
-      {
-        "catalog_nbr": 2053,
-        "subject": "ACTURSCI",
-        "className": "MATH FOR FINANCIAL ANALYSIS",
-        "course_info": [
-          {
-            "class_nbr": 1592,
-            "start_time": "11:30 AM",
-            "descrlong": "Prerequisite(s):1.0 course or two 0.5 courses at the 1000 level or higher from Applied Mathematics, Calculus, or Mathematics.",
-            "end_time": "12:30 PM",
-            "campus": "Main",
-            "facility_ID": "NCB-113",
-            "days": [
-              "M",
-              "W",
-              "F"
-            ],
-            "instructors": [],
-            "class_section": "001",
-            "ssr_component": "LEC",
-            "enrl_stat": "Full",
-            "descr": ""
-          }
-        ],
-        "catalog_description": "Simple and compound interest, annuities, amortization, sinking funds, bonds, bond duration, depreciation, capital budgeting, probability, mortality tables, life annuities, life insurance, net premiums and expenses. Cannot be taken for credit in any module in Statistics or Actuarial Science, Financial Modelling or Statistics, other than the minor in Applied Financial Modeling.\n\nAntirequisite(s): Actuarial Science 2553A/B.\n\nExtra Information: 3 lecture hours."
-      },
-      {
-        "catalog_nbr": "2427B",
-        "subject": "ACTURSCI",
-        "className": "LONG TERM ACTUARIAL MATH I",
-        "course_info": [
-          {
-            "class_nbr": 2663,
-            "start_time": "12:30 PM",
-            "descrlong": "Prerequisite(s): A minimum mark of 60% in each of Actuarial Science 2553A/B, either Calculus 2402A/B or Calculus 2502A/B, and Statistical Sciences 2857A/B. Restricted to students enrolled in any Actuarial Science module.",
-            "end_time": "1:30 PM",
-            "campus": "Main",
-            "facility_ID": "MC-105B",
-            "days": [
-              "M",
-              "W",
-              "F"
-            ],
-            "instructors": [],
-            "class_section": "001",
-            "ssr_component": "LEC",
-            "enrl_stat": "Not full",
-            "descr": ""
-          }
-        ],
-        "catalog_description": "Models for the time until death, single life annuity and life insurance present values and their probability distributions; introduction to equivalence principle and premium calculations.\n\nExtra Information: 3 lecture hours, 1 tutorial hour."
-      },
-      {
-        "catalog_nbr": "2553A",
-        "subject": "ACTURSCI",
-        "className": "MATHEMATICS OF FINANCE",
-        "course_info": [
-          {
-            "class_nbr": 1494,
-            "start_time": "9:30 AM",
-            "descrlong": "Prerequisite(s): A minimum mark of 60% in Calculus 1501A/B or Applied Mathematics 1413, or Calculus 1301A/B with a minimum mark of 85%.",
-            "end_time": "10:30 AM",
-            "campus": "Main",
-            "facility_ID": "WSC-55",
-            "days": [
-              "M",
-              "W",
-              "F"
-            ],
-            "instructors": [],
-            "class_section": "001",
-            "ssr_component": "LEC",
-            "enrl_stat": "Not full",
-            "descr": "PRIORITY TO STUDENTS ENROLLED IN A MODULE OFFERED BY THE DEPARTMENTS OF APPLIED MATHEMATICS; MATHEMATICS; AND STATISTICAL AND ACTUARIAL SCIENCES."
-          }
-        ],
-        "catalog_description": "Time value of money, accumulation and discount functions, effective rates of interest and discount and present values, as applied to annuities and other financial products, and/or applications including loan repayment schedules and methods.\n\nAntirequisite(s): Actuarial Science 2053.\n\nExtra Information: 3 lecture hours, 1 tutorial hour."
-      },
-      {
-        "catalog_nbr": "3424B",
-        "subject": "ACTURSCI",
-        "className": "SHORT TERM ACTUARIAL MATH I",
-        "course_info": [
-          {
-            "class_nbr": 2811,
-            "start_time": "9:30 AM",
-            "descrlong": "Prerequisite(s): A minimum mark of 60% in Statistical Sciences 3657A/B. Restricted to students enroled in any Actuarial Science module, or those registered in the Honours Specialization module in Statistics or the Honours Specialization in Financial Modelling module.",
-            "end_time": "10:30 AM",
-            "campus": "Main",
-            "facility_ID": "AHB-1B02",
-            "days": [
-              "M",
-              "W",
-              "F"
-            ],
-            "instructors": [],
-            "class_section": "001",
-            "ssr_component": "LEC",
-            "enrl_stat": "Not full",
-            "descr": ""
-          }
-        ],
-        "catalog_description": "Insurance loss frequency and severity models; aggregate loss models; risk measures; ruin theory; coverage modifications.\n\nExtra Information: 3 lecture hours."
-      },
-      {
-        "catalog_nbr": "3429A",
-        "subject": "ACTURSCI",
-        "className": "LONG TERM ACTUARIAL MATH II",
-        "course_info": [
-          {
-            "class_nbr": 2812,
-            "start_time": "10:30 AM",
-            "descrlong": "Prerequisite(s): A minimum mark of 60% in each of Actuarial Science 2427A/B and Statistical Sciences 2858A/B.\nCorequisite(s): Statistical Sciences 3657A/B.",
-            "end_time": "11:30 AM",
-            "campus": "Main",
-            "facility_ID": "SEB-2100",
-            "days": [
-              "M",
-              "W",
-              "F"
-            ],
-            "instructors": [],
-            "class_section": "001",
-            "ssr_component": "LEC",
-            "enrl_stat": "Not full",
-            "descr": ""
-          }
-        ],
-        "catalog_description": "Single life annuity and life insurance loss random variables and their distributions, with applications to the analysis of benefit premiums and reserves; survival model estimates; mortality Improvement and longevity models.\n\nExtra Information: 3 lecture hours."
-      },
-    ];
-
-    let selectedComponent = (document.getElementById("componentDropdown") as HTMLInputElement).value;
-    let courseId = (document.getElementById("courseNumber") as HTMLInputElement).value;
-    let subjectInput = (document.getElementById("subjectDropDown") as HTMLInputElement).value;  
-
-    // validate that correct inputs 
+    // validate that input requirements are fulfilled 
       if(subjectInput == ""){
         alert("Please select a specific subject to search");
       }
@@ -250,51 +81,61 @@ export class AppComponent {
         alert("A course ID must be inputted to search");
       }
 
-    console.log(this.dataArray);
+    // more input validation
+      if(courseId.length > 5){
+        alert("No course ID is longer than 5 characters");
+        (document.getElementById("courseNumber") as HTMLInputElement).value = "";
+        return;
+      }
 
-    for(let course of this.dataArray){
+    // find courses matching search params 
+      for(let course of courseData){
 
-      if(selectedComponent == "ALL"){
-        
-        if(subjectInput === course.subject && courseId == course.catalog_nbr){
-          this.matchingCourses.push(course);
+        if(selectedComponent == "ALL"){
+          
+          // todo chop last lette of course code if ALL is enabled? 
+            //if(/* last character is letter*/)
+
+            // let cutSearchCourseId = (course.catalog_nbr).toString().substring(0, course.catalog_nbr.length - 1);
+            // let cutInputtedCourseId =          
+          
+          if(subjectInput === course.subject && (courseId == course.catalog_nbr || course.catalog_nbr.toString().includes(courseId))){
+            matchingCourses.push(course);
+          }
+        }
+        else{
+          if(subjectInput === course.subject &&  (courseId == course.catalog_nbr || course.catalog_nbr.toString().includes(courseId)) && selectedComponent === course.course_info[0].ssr_component){
+            matchingCourses.push(course);
+          }
         }
       }
+
+    // verify some courses matched, if not alert user
+      if(matchingCourses.length == 0){
+        alert("No courses match inputted search fields");
+      }
       else{
-        if(subjectInput === course.subject && courseId == course.catalog_nbr && selectedComponent === course.course_info[0].ssr_component){
-          this.matchingCourses.push(course);
-      }
+        console.log(matchingCourses);
       }
 
-    }
-
-    if(this.matchingCourses.length == 0){
-      alert("No courses match inputted search fields");
-    }
-    else{
-      console.log(this.matchingCourses);
-    }
-
-
-    // parse recieved data to object 
-    // dont need to do this apparently? 
-
-    // once done w object return it
-    //this.data = jsonData; // don't return, just set global class object to data
-    this.data = this.matchingCourses;   // fix later
+    // pass data to data member to be rendered in table
+      this.data = matchingCourses;  
   }
 
   searchSubmitted(){
     // getting dummy data but should be making http request
       this.getData();
+      this.showInfoTable = true;
     
-    // passing data (js object) to be rendered to table
-      let tableCourses = this.renderInfoTable(this.data);
+    // delete all this?
+      // passing data (js object) to be rendered to table
+        //let tableCourses = this.renderInfoTable(this.data);
 
-    // todo after implimenting data fetch make seperate method for rendering table data?
-      this.renderInfoTable(tableCourses);
+      // todo after implimenting data fetch make seperate method for rendering table data?
+        //this.renderInfoTable(tableCourses);
   }
 
+  // delete?
   renderInfoTable(jsData: any){
     this.showInfoTable = true;
   }
@@ -310,7 +151,7 @@ export class AppComponent {
       this.scheduleData[name] = {};
       console.log(this.scheduleData);
     }
-    
+    this.scheduleNameInput = "";
   }
 
   courseSelected(course: object){ 
@@ -370,13 +211,18 @@ export class AppComponent {
    */
   
   days = {Monday: "M", Tuesday: "Tu", Wednesday: "W", Thursday: "Th", Friday: "F"};
-  //daysInDataFormat = ["M", "Tu", "W", "Th", "F"];
   times = ["8:30 AM","9:30 AM","10:30 AM","11:30 AM","12:30 PM","1:30 PM","2:30 PM","3:30 PM","4:30 PM","5:30 PM","6:30 PM","7:30 PM","8:30 PM","9:30 PM"];
 
   renderTimeTable(){
 
+    if((document.getElementById("scheduleDropDown") as HTMLInputElement).value == "none"){
+      alert("You must choose an active schedule to render a time table");
+      return;
+    }
+
     if(this.activeSchedule.length == 0){
       alert("Cannot render an empty schedule");
+      return;
     }
 
     this.renderedSchedule = this.actvieScheduleName;
@@ -437,5 +283,4 @@ export class AppComponent {
   keepOrder = (a, b) => {
     return a;
   }
-
 }
